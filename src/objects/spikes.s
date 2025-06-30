f493cb79b966645fc2d5144ef00157df932a393f
Danny Coulombe <contact@dannycoulombe.com>
2025-06-30 09:19:19 -0400

    progress

--- src/objects/spikes.s
+++ src/objects/spikes.s
@@ -1,11 +1,78 @@
-SpikesObject_Init:
+SPIKE_DATA_DELAY = 5
+SPIKE_DATA_INTERVAL = 6
+SPIKE_MEM_COUNTER = 0
+SPIKE_MEM_FLAG = 1
+SPIKE_MEM_FLAG_RAISED = 1 << 0
+SPIKE_MEM_FLAG_STATE_CHANGED = 1 << 7
+
+SPIKE_METATILE_RAISED = $8C
+SPIKE_METATILE_LOWER = $60
+
+; --------------------------------------
+; Hooks
+SpikesObject_Mounted:
+
+  ; Initialize with delay
+  LDA_ObjData #SPIKE_DATA_DELAY
+  STA_ObjMem SPIKE_MEM_COUNTER
+
   rts
 
 SpikesObject_Frame:
+
+  ; Jump to matching sub-routine if flag match count
+  BEQ_ObjFlagSet SPIKE_MEM_FLAG, #SPIKE_MEM_FLAG_RAISED, :+++
+    JSR_ObjMemEqualsVal SpikesObject_Lower, SPIKE_MEM_COUNTER, #60
+    jmp :+++
+  :
+    JSR_ObjMemEqualsData SpikesObject_Raise, SPIKE_MEM_COUNTER, #SPIKE_DATA_INTERVAL
+  :
+
+  ; Increment counter
+  INC_ObjMem SPIKE_MEM_COUNTER
+
   rts
 
 SpikesObject_NMI:
+
+  ; Switch between lower and raised if state changed
+  BEQ_ObjFlagSet SPIKE_MEM_FLAG, #SPIKE_MEM_FLAG_STATE_CHANGED, :++++
+    ObjMemSetBit SPIKE_MEM_FLAG, SPIKE_MEM_FLAG_STATE_CHANGED, 0
+    BEQ_ObjFlagSet SPIKE_MEM_FLAG, #SPIKE_MEM_FLAG_RAISED, :++
+      SetMetatile $2ACC, #SPIKE_METATILE_RAISED
+      rts
+    :
+      SetMetatile $2ACC, #SPIKE_METATILE_LOWER
+  :
+  rts
+
+SpikesObject_Interaction:
   rts
 
 SpikesObject_Collision:
+  rts
+
+SpikesObject_Destroyed:
+  rts
+
+; --------------------------------------
+; Actions
+SpikesObject_Raise:
+
+  jsr Sound::RisingSpike
+
+  lda #0
+  STA_ObjMem SPIKE_MEM_COUNTER
+  ObjMemSetBit SPIKE_MEM_FLAG, SPIKE_MEM_FLAG_STATE_CHANGED, 1
+  ObjMemSetBit SPIKE_MEM_FLAG, SPIKE_MEM_FLAG_RAISED, 1
+  rts
+
+SpikesObject_Lower:
+
+  jsr Sound::FallingSpike
+
+  lda #0
+  STA_ObjMem SPIKE_MEM_COUNTER
+  ObjMemSetBit SPIKE_MEM_FLAG, SPIKE_MEM_FLAG_STATE_CHANGED, 1
+  ObjMemSetBit SPIKE_MEM_FLAG, SPIKE_MEM_FLAG_RAISED, 0
   rts
